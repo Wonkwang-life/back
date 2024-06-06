d626db6fc7f6e550a0cbd85e3bb1c8feded7ac9c
nemokoala <fdxguy2@gmail.com>
2024-06-06 12:11:10 +0900

    feat: xss공격 방지를 위한 글 추가/수정 시 일부 단어 필터링 후 제거

--- src/main/java/com/wonkwang/health/service/PostService.java
+++ src/main/java/com/wonkwang/health/service/PostService.java
@@ -23,6 +23,8 @@ public class PostService {
     private final S3Service s3Service;
 
     public Long createPost(PostDTO postDTO) {
+        String sanitizedContent = sanitize(postDTO.getContent()); //악성 스크립트 제거
+        postDTO.setContent(sanitizedContent);
         Post post = new Post(postDTO);
         Post savedPost = postRepository.save(post);
         return savedPost.getId();
@@ -31,6 +33,9 @@ public class PostService {
     public void updatePost(Long postId, PostDTO postDTO) {
         Post findPost = postRepository.findPostWithUrlsById(postId)
                 .orElseThrow(() -> new EntityNotFoundException("해당 글이 없습니다."));
+
+        String sanitizedContent = sanitize(postDTO.getContent()); //악성 스크립트 제거
+        postDTO.setContent(sanitizedContent);
 
         // 기존 이미지 URL 리스트
         List<String> existingImageUrls = findPost.getImageUrls();
@@ -77,5 +82,17 @@ public class PostService {
         postRepository.delete(findPost);
         log.info("{} 글 삭제 완료", postId);
     }
+
+    private String sanitize(String content) {
+        // 정규 표현식을 사용하여 여러 태그와 속성을 제거
+        String cleanContent = content.replaceAll("(?i)<script.*?>.*?</script.*?>", ""); // script 태그 제거
+        cleanContent = cleanContent.replaceAll("(?i)<img.*?>", "[removed]"); // img 태그 제거
+        cleanContent = cleanContent.replaceAll("(?i)\\s+on\\w+\\s*=\\s*\"[^\"]*\"", ""); // 이벤트 핸들러 속성 제거
+        cleanContent = cleanContent.replaceAll("(?i)\\s+on\\w+\\s*=\\s*'[^']*'", ""); // 이벤트 핸들러 속성 제거 (단일 인용부호)
+        cleanContent = cleanContent.replaceAll("(?i)\\s+javascript\\s*:\\s*[^\"]*\"", ""); // javascript: 속성 제거
+        cleanContent = cleanContent.replaceAll("(?i)\\s+javascript\\s*:\\s*[^']*'", ""); // javascript: 속성 제거 (단일 인용부호)
+        return cleanContent;
+    }
+
 }
 
